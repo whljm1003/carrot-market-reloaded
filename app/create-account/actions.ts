1a374e36bc4e13a6ab168b194a40b3ae9f23f92a
jungminLee <whljm1003@gmail.com>
2024-12-02 14:21:00 +0900

    4.3 Transformation

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -1,5 +1,10 @@
 "use server";
 import { z } from "zod";
+
+const passwordRegex = new RegExp(
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*?[#?!@$%^&*-]).+$/
+);
+
 const formSchema = z
   .object({
     username: z
@@ -8,12 +13,22 @@ const formSchema = z
         required_error: "where is my username????",
       })
       .min(3, "Way too short!!!")
-      .max(10, "That is too looooong!")
-      .refine((username) => !username.includes("potato")),
+      .trim()
+      .transform((username) => `🔥 ${username} 🔥`)
+      .refine(
+        (username) => !username.includes("potato"),
+        "No potatoes allowed!"
+      ),
 
-    email: z.string().email(),
-    password: z.string().min(10),
-    confirm_password: z.string().min(10),
+    email: z.string().email().toLowerCase(),
+    password: z
+      .string()
+      .min(4)
+      .regex(
+        passwordRegex,
+        "Passwords must contain at least one UPPERCASE, lowercase, number and special characters #?!@$%^&*-"
+      ),
+    confirm_password: z.string().min(4),
   })
   .superRefine(({ password, confirm_password }, ctx) => {
     if (password !== confirm_password) {
@@ -35,5 +50,7 @@ export async function createAccount(prevState: any, formData: FormData) {
   const result = formSchema.safeParse(data);
   if (!result.success) {
     return result.error.flatten();
+  } else {
+    console.log(result.data);
   }
 }
