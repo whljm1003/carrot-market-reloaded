4a61e8efd853ce169d8edac8ce0687756cd2f954
jungminLee <whljm1003@gmail.com>
2024-12-02 13:20:23 +0900

    4.0 Introduction to Zod

--- app/login/actions.ts
+++ app/login/actions.ts
@@ -1,8 +1,8 @@
 "use server";
 
 export async function handleForm(prevState: any, formData: FormData) {
-  console.log(prevState);
-  await new Promise((resolve) => setTimeout(resolve, 5000));
+  // console.log(prevState);
+  // await new Promise((resolve) => setTimeout(resolve, 5000));
   return {
     errors: ["wrong password", "password too short"],
   };
--- components/form-input.tsx
+++ components/form-input.tsx
@@ -2,21 +2,21 @@ interface FormInputProps {
   type: string;
   placeholder: string;
   required: boolean;
-  errors: string[];
+  errors?: string[];
   name: string;
 }
 export default function FormInput({
   type,
   placeholder,
   required,
-  errors,
+  errors = [],
   name,
 }: FormInputProps) {
   return (
     <div className="flex flex-col gap-2">
       <input
         name={name}
-        className="bg-transparent rounded-md w-full h-10 focus:outline-none ring-2 focus:ring-4 transition ring-neutral-200 focus:ring-orange-500 border-none placeholder:text-neutral-400"
+        className="pl-2 bg-transparent rounded-md w-full h-10 focus:outline-none ring-2 focus:ring-4 transition ring-neutral-200 focus:ring-orange-500 border-none placeholder:text-neutral-400"
         type={type}
         placeholder={placeholder}
         required={required}
