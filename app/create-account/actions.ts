7ce723b8c34df10899c43ff469200a392d5c96c4
jungminLee <whljm1003@gmail.com>
2024-12-02 13:23:39 +0900

    4.1 Validation Errors

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -1,6 +1,11 @@
 "use server";
 import { z } from "zod";
-const usernameSchema = z.string().min(5).max(10);
+const formSchema = z.object({
+  username: z.string(),
+  email: z.string().email(),
+  password: z.string().min(10),
+  confirm_password: z.string().min(10),
+});
 export async function createAccount(prevState: any, formData: FormData) {
   const data = {
     username: formData.get("username"),
@@ -8,5 +13,8 @@ export async function createAccount(prevState: any, formData: FormData) {
     password: formData.get("password"),
     confirm_password: formData.get("confirm_password"),
   };
-  usernameSchema.parse(data.username);
+  const result = formSchema.safeParse(data);
+  if (!result.success) {
+    return result.error.flatten();
+  }
 }
