3bcd17feecea1150916bc3e693714ed2c7662ff1
jungminLee <whljm1003@gmail.com>
2024-12-06 10:48:40 +0900

    6.2 Password Hashing

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -6,6 +6,7 @@ import {
 } from "@/lib/constants";
 import db from "@/lib/db";
 import { z } from "zod";
+import bcrypt from "bcrypt";
 
 const checkUsername = (username: string) => !username.includes("potato");
 
@@ -82,8 +83,18 @@ export async function createAccount(prevState: any, formData: FormData) {
   if (!result.success) {
     return result.error.flatten();
   } else {
-    // hash password
-    // save the user to db
+    const hashedPassword = await bcrypt.hash(result.data.password, 12);
+    const user = await db.user.create({
+      data: {
+        username: result.data.username,
+        email: result.data.email,
+        password: result.data.password,
+      },
+      select: {
+        id: true,
+      },
+    });
+
     // log the user in
     // redirect "/home"
   }
