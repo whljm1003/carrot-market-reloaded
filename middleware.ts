b4393fb342e24fee93f912694971019d90863682
jungminLee <whljm1003@gmail.com>
2024-12-15 12:40:01 +0900

    6.10 Matcher

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,10 @@
-import { NextRequest, NextResponse } from "next/server";
-import getSession from "./lib/session";
+import { NextRequest } from "next/server";
 
 export async function middleware(request: NextRequest) {
-  // console.log("i am middleware");
-  const session = await getSession();
-  console.log(session);
-  if (request.nextUrl.pathname === "/profile") {
-    return NextResponse.redirect(new URL("/", request.url));
-  }
+  console.log("hello");
 }
+
+// matcher를 사용하면 matcher에 지정한 특정 경로들에서만 미들웨어가 실행되도록 할 수 있습니다.
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
