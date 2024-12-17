5c413664ed254392ab54687ff3cfd00bebde1e1c
jungminLee <whljm1003@gmail.com>
2024-12-17 14:50:27 +0900

    8.3 Product Component

--- app/(tabs)/products/page.tsx
+++ app/(tabs)/products/page.tsx
@@ -1,15 +1,29 @@
-import React from "react";
+import ListProduct from "@/components/list-product";
+import db from "@/lib/db";
+import getSession from "@/lib/session";
+import React, { useEffect } from "react";
 
 async function getProduct() {
-  await new Promise((resolve) => setTimeout(resolve, 10000));
+  const products = await db.product.findMany({
+    select: {
+      title: true,
+      price: true,
+      created_at: true,
+      photo: true,
+      id: true,
+    },
+  });
+  return products;
 }
 
 export default async function Products() {
   const products = await getProduct();
 
   return (
-    <div>
-      <h1 className="text-white text-4xl">Products!</h1>
+    <div className="p-5 flex flex-col gap-5">
+      {products.map((product) => (
+        <ListProduct key={product.id} {...product} />
+      ))}
     </div>
   );
 }
