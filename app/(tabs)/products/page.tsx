24f50bcef4e8cf531f5baf06efd00745cee0adb4
jungminLee <whljm1003@gmail.com>
2024-12-17 14:33:34 +0900

    8.2 Skeletons

--- app/(tabs)/products/page.tsx
+++ app/(tabs)/products/page.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
-type Props = {};
+async function getProduct() {
+  await new Promise((resolve) => setTimeout(resolve, 10000));
+}
 
-export default function Products({}: Props) {
-  return <div>Products</div>;
+export default async function Products() {
+  const products = await getProduct();
+
+  return (
+    <div>
+      <h1 className="text-white text-4xl">Products!</h1>
+    </div>
+  );
 }
