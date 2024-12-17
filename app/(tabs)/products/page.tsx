8ea0dd293552ccad9ecaf1da5b474ccda4f12817
jungminLee <whljm1003@gmail.com>
2024-12-17 15:34:52 +0900

    8.7 Pagination Actions

--- app/(tabs)/products/page.tsx
+++ app/(tabs)/products/page.tsx
@@ -1,9 +1,9 @@
-import ListProduct from "@/components/list-product";
+import ProductList from "@/components/product-list";
 import db from "@/lib/db";
-import getSession from "@/lib/session";
-import React, { useEffect } from "react";
+import { Prisma } from "@prisma/client";
+import React from "react";
 
-async function getProduct() {
+async function getInitialProducts() {
   const products = await db.product.findMany({
     select: {
       title: true,
@@ -12,18 +12,24 @@ async function getProduct() {
       photo: true,
       id: true,
     },
+    take: 1,
+    orderBy: {
+      created_at: "desc",
+    },
   });
   return products;
 }
 
+export type InitialProducts = Prisma.PromiseReturnType<
+  typeof getInitialProducts
+>;
+
 export default async function Products() {
-  const products = await getProduct();
+  const initialProducts = await getInitialProducts();
 
   return (
-    <div className="p-5 flex flex-col gap-5">
-      {products.map((product) => (
-        <ListProduct key={product.id} {...product} />
-      ))}
+    <div>
+      <ProductList initialProducts={initialProducts} />
     </div>
   );
 }
