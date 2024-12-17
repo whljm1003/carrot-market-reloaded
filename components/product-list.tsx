bfe5c27fe50d0e2496352ca769dbd2d0c0fc444d
jungminLee <whljm1003@gmail.com>
2024-12-17 15:47:15 +0900

    8.9 Infinite Scrolling

--- components/product-list.tsx
+++ components/product-list.tsx
@@ -2,7 +2,7 @@
 
 import { getMoreProducts } from "@/app/(tabs)/products/actions";
 import { InitialProducts } from "@/app/(tabs)/products/page";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import ListProduct from "./list-product";
 
 interface ProductListProps {
@@ -14,34 +14,59 @@ export default function ProductList({ initialProducts }: ProductListProps) {
   const [isLoading, setIsLoading] = useState(false);
   const [page, setPage] = useState(0);
   const [isLastPage, setIsLastPage] = useState(false);
-  const onLoadMoreClick = async () => {
-    setIsLoading(true);
-    const newProducts = await getMoreProducts(page + 1);
-    if (newProducts.length !== 0) {
-      setPage((prev) => prev + 1);
-      setProducts((prev) => [...prev, ...newProducts]);
-    } else {
-      setIsLastPage(true);
+  const trigger = useRef<HTMLSpanElement>(null);
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      async (
+        entries: IntersectionObserverEntry[],
+        observer: IntersectionObserver
+      ) => {
+        const element = entries[0];
+        /* 
+          isIntersecting -> true : trigger tag 조회
+        */
+        if (element.isIntersecting && trigger.current) {
+          observer.unobserve(trigger.current);
+          setIsLoading(true);
+          const newProducts = await getMoreProducts(page + 1);
+          if (newProducts.length !== 0) {
+            setPage((prev) => prev + 1);
+            setProducts((prev) => [...prev, ...newProducts]);
+          } else {
+            setIsLastPage(true);
+          }
+          setIsLoading(false);
+        }
+      },
+      {
+        threshold: 1.0,
+      }
+    );
+    if (trigger.current) {
+      observer.observe(trigger.current);
     }
-    setIsLoading(false);
-  };
+    // unmount 시 observer 제거
+    return () => {
+      observer.disconnect();
+    };
+  }, [page]);
 
   return (
     <div className="p-5 flex flex-col gap-5">
       {products.map((product) => (
         <ListProduct key={product.id} {...product} />
       ))}
-      {isLastPage ? (
-        "No more items"
-      ) : (
-        <button
-          onClick={onLoadMoreClick}
-          disabled={isLoading}
-          className="text-sm font-semibold bg-orange-500 w-fit mx-auto px-3 py-2 rounded-md hover:opacity-90 active:scale-95"
+      {!isLastPage ? (
+        <span
+          ref={trigger}
+          style={{
+            marginTop: `${page + 1 * 900}vh`,
+          }}
+          className="mb-96 text-sm font-semibold bg-orange-500 w-fit mx-auto px-3 py-2 rounded-md hover:opacity-90 active:scale-95"
         >
           {isLoading ? "로딩 중" : "Load more"}
-        </button>
-      )}
+        </span>
+      ) : null}
     </div>
   );
 }
