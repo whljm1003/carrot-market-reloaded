5a6113f687eb2b0bad8fd55b14e279a41c455c73
leejungmin <whljm1003@gmail.com>
2025-03-05 14:10:22 +0900

    13.3 Messages

---
+++ app/chats/[id]/page.tsx
@@ -0,0 +1,64 @@
+import ChatMessagesList from "@/components/chat-messages-list";
+import db from "@/lib/db";
+import getSession from "@/lib/session";
+import { Prisma } from "@prisma/client";
+import { notFound } from "next/navigation";
+import React from "react";
+
+async function getRoom(id: string) {
+  const room = await db.chatRoom.findUnique({
+    where: {
+      id,
+    },
+    include: {
+      users: {
+        select: { id: true },
+      },
+    },
+  });
+  if (room) {
+    const session = await getSession();
+    const canSee = Boolean(room.users.find((user) => user.id === session.id!));
+    if (!canSee) {
+      return null;
+    }
+  }
+  return room;
+}
+
+async function getMessages(chatRoomId: string) {
+  const messages = await db.message.findMany({
+    where: {
+      chatRoomId,
+    },
+    select: {
+      id: true,
+      payload: true,
+      created_at: true,
+      userId: true,
+      user: {
+        select: {
+          username: true,
+          avatar: true,
+        },
+      },
+    },
+  });
+  return messages;
+}
+
+export type InitialChatMessages = Prisma.PromiseReturnType<typeof getMessages>;
+
+export default async function ChatRoom({ params }: { params: { id: string } }) {
+  const room = await getRoom(params.id);
+  if (!room) {
+    return notFound();
+  }
+
+  const initialMessages = await getMessages(room.id);
+  const session = await getSession();
+
+  return (
+    <ChatMessagesList userId={session.id!} initialMessages={initialMessages} />
+  );
+}
