f2d37cc9302bd306093d2ef2c73a2a12cc8de1f4
leejungmin <whljm1003@gmail.com>
2025-03-11 22:50:31 +0900

    14.1 Live Input

--- app/(tabs)/live/page.tsx
+++ app/(tabs)/live/page.tsx
@@ -1,5 +1,16 @@
+import { PlusIcon } from "@heroicons/react/24/solid";
+import Link from "next/link";
 import React from "react";
 
 export default function Live() {
-  return <div>Live</div>;
+  return (
+    <div>
+      <Link
+        href={"streams/add"}
+        className="bg-orange-500 flex items-center justify-center rounded-full size-16 fixed bottom-24 right-8 text-white transition-colors hover:bg-orange-400"
+      >
+        <PlusIcon className="size-10" />
+      </Link>
+    </div>
+  );
 }
