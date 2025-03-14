a8b536c47dcdd8a13b450c8346e6b0e7c07e8396
leejungmin <whljm1003@gmail.com>
2025-03-14 17:50:39 +0900

    15.4 Logging

--- app/extras/[[...potato]]/page.tsx
+++ app/extras/[[...potato]]/page.tsx
@@ -1,10 +1,30 @@
-export default function Extras({ params }: { params: { potato: string[] } }) {
-  console.log(params);
+import { revalidatePath } from "next/cache";
+
+async function getData() {
+  const data = await fetch(
+    "https://nomad-movies.nomadcoders.workers.dev/movies"
+  );
+}
+
+export default async function Extras({
+  params,
+}: {
+  params: { potato: string[] };
+}) {
+  await getData();
+  const action = async () => {
+    "use server";
+    revalidatePath("extras");
+  };
+
   return (
     <div className="flex flex-col gap-3 py-10">
       <h1 className="text-6xl font-metallica">metallica!</h1>
       <h1 className="text-6xl font-rubick">Rubick!</h1>
       <h2 className="font-roboto">So much more to learn!</h2>
+      <form action={action}>
+        <button>revalidate</button>
+      </form>
     </div>
   );
 }
